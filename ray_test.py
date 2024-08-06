e1f953377d56158fb1780b6b21ea70acba1b8163
Emanuele Petriglia <inbox@emanuelepetriglia.com>
2024-08-06 16:10:01 +0200

    Finalize ray_test for MARL

--- ray_test.py
+++ ray_test.py
@@ -10,12 +10,11 @@ from ray.tune.registry import register_env
 # GuessTheNumberGame is not registered, I have to do it manually.
 register_env("GuessTheNumberGame", lambda env_config: GuessTheNumberGame(config=env_config))
 
-# The first agent can choose a number between 0 and 1 million. The second agent
-# should find this number in less than 20 steps (13/14 steps using binary
-# search).
-#env_config = {"max_number": 1000000, "max_steps": 20}
+# The first agent can choose a number between 0 and 100. The second agent should
+# find this number in less than 20 steps (13/14 steps using binary search).
 env_config = {"max_number": 100, "max_steps": 20}
 
+# Fixed seed.
 seed = 42
 
 # This experiment has two policies, one for each player. The first player
@@ -28,10 +27,14 @@ policies = {"policy_0": PolicySpec(policy_class=RandomPolicy),
             "policy_1": PolicySpec()
             }
 
-# This function is called at each step to assign the agent to a policy. In this
-# case, each agent has a fixed corresponding policy.
-policy_mapping_fn = lambda agent_id, episode, worker, **kwargs: f"policy_{agent_id}"
 
+def policy_mapping_fn(agent_id, episode, worker, **kwargs):
+    '''This function is called at each step to assign the agent to a policy. In
+    this case, each agent has a fixed corresponding policy.'''
+    return f"policy_{agent_id}"
+
+
+# Experiment config.
 ppo_config = (PPOConfig()
               .environment(env="GuessTheNumberGame", env_config=env_config, disable_env_checking=True)
               .framework("torch")
@@ -44,13 +47,14 @@ ppo_config = (PPOConfig()
                            policies_to_train=["policy_1"])
               )
 
+# Build the experiment.
 ppo_algo = ppo_config.build()
 
-for iteration in range(2000):
+# Run the training phase for 10 iterations.
+for iteration in range(10):
     print(f"Iteration nr. {iteration}")
     result = ppo_algo.train()
-    #print(pretty_print(result))
 
+# Do a final evaluation.
 print("Evaluation")
-
 print(pretty_print(ppo_algo.evaluate()))
