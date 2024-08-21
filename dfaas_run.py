130b065e8cd95327640a2f2804e92630465f2582
Emanuele Petriglia <inbox@emanuelepetriglia.com>
2024-08-21 12:27:13 +0200

    Use custom folder to save multi-agent experiments

--- dfaas_run.py
+++ dfaas_run.py
@@ -1,6 +1,10 @@
+from pathlib import Path
+from datetime import datetime
+import json
+
 from ray.rllib.algorithms.ppo import PPOConfig
 from ray.rllib.policy.policy import PolicySpec
-from ray.tune.logger import pretty_print
+from ray.tune.logger import UnifiedLogger
 
 from dfaas_env import DFaaS  # noqa: F401
 
@@ -36,13 +40,25 @@ seed = 42
 
 env_config = {'seed': 42}
 
+# Create the results directory to override Ray's default.
+logdir = Path.cwd() / "results"
+logdir = logdir / Path(f"DFAAS-MA_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}")
+logdir.mkdir(parents=True, exist_ok=True)
+print(f"DFAAS experiment directory created at {logdir.as_posix()!r}")
+
+
+# This function is called by Ray when creating the logger for the experiment.
+def logger_creator(config):
+    return UnifiedLogger(config, logdir.as_posix())
+
+
 # Experiment config.
 ppo_config = (PPOConfig()
               .environment(env="DFaaS", env_config=env_config)
               .framework("torch")
-              .rollouts(num_rollout_workers=0)  # Local worker.
+              .rollouts(num_rollout_workers=0)  # Only a local worker.
               .evaluation(evaluation_interval=None)
-              .debugging(seed=seed)
+              .debugging(logger_creator=logger_creator, seed=seed)
               .resources(num_gpus=1)
               .multi_agent(policies=policies,
                            policy_mapping_fn=policy_mapping_fn)
@@ -51,11 +67,13 @@ ppo_config = (PPOConfig()
 # Build the experiment.
 ppo_algo = ppo_config.build()
 
-# Run the training phase for 10 iterations.
+# Run the training phase for n iterations.
 for iteration in range(1):
-    print(f"Iteration nr. {iteration}")
+    print(f"Iteration {iteration}")
     result = ppo_algo.train()
 
 # Do a final evaluation.
-print("Evaluation")
-print(pretty_print(ppo_algo.evaluate()))
+print("Final evaluation")
+evaluation = ppo_algo.evaluate()
+eval_file = logdir / "final_evaluation.json"
+eval_file.write_text(json.dumps(evaluation), encoding="utf8")
