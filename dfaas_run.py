d26ce3686fad2b029226114f549efb0b99494264
Emanuele Petriglia <inbox@emanuelepetriglia.com>
2024-08-21 11:10:04 +0200

    Specify action/obs space in PolicySpec

--- dfaas_run.py
+++ dfaas_run.py
@@ -4,13 +4,27 @@ from ray.tune.logger import pretty_print
 
 from dfaas_env import DFaaS  # noqa: F401
 
+tmp_env = DFaaS()
+
+# Each agent in the DFaaS network has the same action and observation space, but
+# this can be changed in the future. A PolicySpec is required to specify this
+# information for each policy used in this experiment.
+#
+# See this thread: https://discuss.ray.io/t/multi-agent-where-does-the-first-structure-comes-from/7010/5
+policy_spec = PolicySpec(policy_class=None,
+                         observation_space=tmp_env.observation_space["node_0"],
+                         action_space=tmp_env.action_space["node_0"],
+                         config=None)
+
 # Each agent has its own policy (policy_node_X in policy -> node_X in the env).
 #
 # Note that if no option is given to PolicySpec, it will inherit the
 # configuration/algorithm from the main configuration.
-policies = {"policy_node_0": PolicySpec(), "policy_node_1": PolicySpec()}
+policies = {"policy_node_0": policy_spec, "policy_node_1": policy_spec}
 
 
+# This function is called by Ray to determine which policy to use for an agent
+# returned in the observation dictionary by step() or reset().
 def policy_mapping_fn(agent_id, episode, worker, **kwargs):
     '''This function is called at each step to assign the agent to a policy. In
     this case, each agent has a fixed corresponding policy.'''
@@ -24,7 +38,7 @@ env_config = {'seed': 42}
 
 # Experiment config.
 ppo_config = (PPOConfig()
-              .environment(env="DFaaS", env_config=env_config, disable_env_checking=True)
+              .environment(env="DFaaS", env_config=env_config)
               .framework("torch")
               .rollouts(num_rollout_workers=0)  # Local worker.
               .evaluation(evaluation_interval=None)
