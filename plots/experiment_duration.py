cf7ed10e6afe41c5612b545a09306377f7733a88
Emanuele Petriglia <inbox@emanuelepetriglia.com>
2024-09-16 11:31:30 +0200

    Add experiment duration plot

---
+++ plots/experiment_duration.py
@@ -0,0 +1,56 @@
+# This Python script generates a graph showing the duration times of different
+# experiments run by changing the GPU enable/disable and the number of rollout
+# workers.
+#
+# The raw data is manually collected and hardcoded into this script.
+from pathlib import Path
+import argparse
+
+import matplotlib
+import matplotlib.pyplot as plt
+
+
+def make(output_dir):
+    plots_dir = output_dir / "experiment_duration"
+    plots_dir.mkdir(parents=True, exist_ok=True)
+
+    # Each key is a type of experiment ("gpu" and "no_gpu"). Each value is a
+    # list of 9 entries for each (from 0 rollout workers to 8). Each item is the
+    # number of minutes taken from the corresponding experiment.
+    data = {}
+    data["gpu"] = [139, 126, 101, 89, 85, 42, 80, 78, 77]
+
+    # Make the plot.
+    fig = plt.figure(dpi=600, layout="constrained")
+    ax = fig.subplots()
+
+    ax.plot(data["gpu"], label="With GPU", color="g", linewidth=2, marker="o")
+
+    ax.set_title("Experiment duration (24 CPU, 64 GB RAM, Testla T4)")
+    ax.set_xlabel("Rollout workers")
+    ax.set_ylabel("Minutes")
+
+    ax.grid(axis="both")
+    ax.set_axisbelow(True)  # By default the axis is over the content.
+
+    ax.legend()
+
+    # Save the plot.
+    path = plots_dir / "experiment_duration.pdf"
+    fig.savefig(path)
+    plt.close(fig)
+    print(f"{path.as_posix()!r}")
+
+
+if __name__ == "__main__":
+    matplotlib.use("pdf", force=True)
+
+    # Create parser and parse arguments.
+    parser = argparse.ArgumentParser(prog="experiment_duration")
+
+    parser.add_argument(dest="output_dir",
+                        help="Where to save the plot")
+
+    args = parser.parse_args()
+
+    make(Path(args.output_dir))
