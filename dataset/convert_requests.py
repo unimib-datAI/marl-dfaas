ae876955e84a4c44944074582720e311815fa771
Emanuele Petriglia <inbox@emanuelepetriglia.com>
2024-09-18 12:36:02 +0200

    Convert from 1440 steps to 288 (1 -> 5 minutes)

--- dataset/convert_requests.py
+++ dataset/convert_requests.py
@@ -8,6 +8,7 @@ import pandas as pd
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 
 # Add the current directory (where Python is called) to sys.path. This is
 # required to load modules in the project root directory (like dfaas_utils.py).
@@ -49,9 +50,11 @@ def make(output_dir, data):
 
     ax.grid(axis="both")
     ax.set_axisbelow(True)  # By default the axis is over the content.
+    ax.yaxis.set_major_locator(ticker.MultipleLocator(10))  # Ticks every 10 requests.
+    ax.xaxis.set_major_locator(ticker.MultipleLocator(25))  # Ticks every 25 steps.
 
     # Save the plot.
-    path = plots_dir / f"convert_requests_{data['function']}.pdf"
+    path = plots_dir / f"convert_requests_{data['function']}_scaled.pdf"
     fig.savefig(path)
     plt.close(fig)
     print(f"{path.as_posix()!r}")
@@ -61,14 +64,25 @@ def main(output_dir, data_file, function_hash):
     output_dir.mkdir(parents=True, exist_ok=True)
 
     data = _get_data(data_file, function_hash)
+    invoc = data["invocations"]
 
-    max = 150  # TODO: Get dinamically.
+    # The environment has 288 steps by default (1 step for every 5 minutes to
+    # cover 24 hours). The Azure trace data has, for each function, the number
+    # of invocations for each minute in 24 hours: I need to merge 5 minutes into
+    # one to reduce the length of the original trace.
+    src_steps = 1440
+    minutes = 5
+    groups = src_steps // minutes  # Nr. of steps in the environment.
+    assert invoc.size == src_steps, f"Unknown size = {invoc.size}"
+    invoc = invoc.reshape(groups, minutes).sum(axis=1)
 
-    out = data["invocations"] * (max/data["invocations"].max())
-    out = np.asarray(out, dtype=np.int32)
-    data["invocations"] = out
+    # Scale the values between the range limits of the observation space.
+    src_xp = (invoc.min(), invoc.max())
+    dst_fp = (0, 150)  # TODO: Get dinamically from the environment.
+    assert np.all(np.diff(src_xp) > 0)
+    data["invocations"] = np.interp(invoc, src_xp, dst_fp).astype(np.int32)
 
-    json_path = output_dir / f"function_{data['function']}.json"
+    json_path = output_dir / f"function_{data['function']}_scaled.json"
     dfaas_utils.dict_to_json(data, json_path)
     print(f"{json_path.as_posix()!r}")
 
