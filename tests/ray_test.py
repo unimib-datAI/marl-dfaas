f114d9b1d9686fa6fb7bc306691cacd875d219a9
Emanuele Petriglia <inbox@emanuelepetriglia.com>
2024-12-05 10:55:02 +0100

    Run black code fmt to ray_test.py

--- tests/ray_test.py
+++ tests/ray_test.py
@@ -1,5 +1,6 @@
-# This is a test file to run Ray in a multi-agent environment with PPO
-# algorithm. If it runs, Ray is installed correctly.
+# This is a test file to run Ray RLlib in a multi-agent environment using the
+# PPO algorithm. If it runs, RLlib and its dependencies are installed
+# correctly.
 from ray.rllib.examples.envs.classes.multi_agent import GuessTheNumberGame
 from ray.rllib.algorithms.ppo import PPOConfig
 from ray.rllib.policy.policy import PolicySpec
@@ -8,7 +9,9 @@ from ray.tune.logger import pretty_print
 from ray.tune.registry import register_env
 
 # GuessTheNumberGame is not registered, I have to do it manually.
-register_env("GuessTheNumberGame", lambda env_config: GuessTheNumberGame(config=env_config))
+register_env(
+    "GuessTheNumberGame", lambda env_config: GuessTheNumberGame(config=env_config)
+)
 
 # The first agent can choose a number between 0 and 100. The second agent should
 # find this number in less than 20 steps (13/14 steps using binary search).
@@ -23,31 +26,36 @@ seed = 42
 #
 # Note that if no option is given to PolicySpec, it will inherit the
 # configuration/algorithm from the main configuration.
-policies = {"policy_0": PolicySpec(policy_class=RandomPolicy),
-            "policy_1": PolicySpec()
-            }
+policies = {"policy_0": PolicySpec(policy_class=RandomPolicy), "policy_1": PolicySpec()}
 
 
 def policy_mapping_fn(agent_id, episode, worker, **kwargs):
-    '''This function is called at each step to assign the agent to a policy. In
-    this case, each agent has a fixed corresponding policy.'''
+    """This function is called at each step to assign the agent to a policy. In
+    this case, each agent has a fixed corresponding policy."""
     return f"policy_{agent_id}"
 
 
 # Experiment config.
-ppo_config = (PPOConfig()
-              # By default RLlib uses the new API stack, but I use the old one.
-              .api_stack(enable_rl_module_and_learner=False,enable_env_runner_and_connector_v2=False)
-              .environment(env="GuessTheNumberGame", env_config=env_config, disable_env_checking=True)
-              .framework("torch")
-              .env_runners(num_env_runners=0)  # Sample experiences from the main process.
-              .evaluation(evaluation_interval=None)
-              .debugging(seed=seed)
-              .resources(num_gpus=1)
-              .multi_agent(policies=policies,
-                           policy_mapping_fn=policy_mapping_fn,
-                           policies_to_train=["policy_1"])
-              )
+ppo_config = (
+    PPOConfig()
+    # By default RLlib uses the new API stack, but I use the old one.
+    .api_stack(
+        enable_rl_module_and_learner=False, enable_env_runner_and_connector_v2=False
+    )
+    .environment(
+        env="GuessTheNumberGame", env_config=env_config, disable_env_checking=True
+    )
+    .framework("torch")
+    .env_runners(num_env_runners=0)  # Sample experiences from the main process.
+    .evaluation(evaluation_interval=None)
+    .debugging(seed=seed)
+    .resources(num_gpus=1)
+    .multi_agent(
+        policies=policies,
+        policy_mapping_fn=policy_mapping_fn,
+        policies_to_train=["policy_1"],
+    )
+)
 
 # Build the experiment.
 ppo_algo = ppo_config.build()
