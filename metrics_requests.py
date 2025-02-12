844bf625dadb38ca9145451c35c2f7e6a2bbe73c
Emanuele Petriglia <inbox@emanuelepetriglia.com>
2025-02-12 16:10:03 +0100

    Update metrics_requests to custom env config

--- metrics_requests.py
+++ metrics_requests.py
@@ -2,26 +2,28 @@
 # generated syntetic requests with the given seed and number of episodes.
 #
 # It is used to compare these traces with the real ones.
+from pathlib import Path
 import argparse
 
 import dfaas_env
+import dfaas_utils
 
 import numpy as np
 
 
-def main(type, seed, episodes):
+def main(env_config, seed, num_episodes):
     # Create a dummy env to generate the requests.
-    env = dfaas_env.DFaaS(config={"input_requests_type": type})
+    env = dfaas_env.DFaaS(config=env_config)
     env.reset(seed=seed)  # Set the initial seed.
 
-    # Each episode generates two input requests (if the env has two agents), so
-    # the total number of input requests is greater.
-    length = episodes * len(env.agents)
+    # Each episode generates k series of input requests, where k is the number
+    # of agents.
+    length = num_episodes * len(env.agents)
 
     sum = np.empty(length, dtype=np.int32)
     mean = np.empty(length)
     std = np.empty(length)
-    for episode in range(episodes):
+    for episode in range(num_episodes):
         # Generate new requests.
         env.reset()
 
@@ -36,7 +38,8 @@ def main(type, seed, episodes):
 
             agent_offset += 1
 
-    print(f"Input requests of {type = } with {seed = } for {episodes = }")
+    reqs_type = env.get_config()["input_requests_type"]
+    print(f"Input requests of {reqs_type = } with {seed = } for {num_episodes = }")
     for metric, data in {"sum": sum, "mean": mean, "std": std}.items():
         # Show the result on the standard output. Maybe in the future can be
         # saved to a CSV file.
@@ -53,20 +56,33 @@ if __name__ == "__main__":
     )
 
     parser.add_argument(
-        "type",
-        choices=["synthetic-sinusoidal", "synthetic-normal", "real"],
-        help="Type of input requests to generate",
-    )
-    parser.add_argument(
-        "--seed", default=0, type=int, help="Seed used to generated synthetic requests"
+        "--seeds",
+        nargs="+",
+        default=42,
+        type=int,
+        help="Seeds used to generated synthetic requests",
     )
     parser.add_argument(
         "--episodes", default=10000, type=int, help="Number of episodes to generate"
     )
+    parser.add_argument(
+        "--env-config",
+        type=Path,
+        help="Environment config file (if None uses default config)",
+    )
 
     args = parser.parse_args()
 
-    assert args.seed >= 0, "Seed must be a non-negative integer"
+    for seed in args.seeds:
+        assert seed >= 0, f"Seed must be a non-negative integer, found {seed}"
     assert args.episodes > 0, "Episodes to generate must be a positive integer"
 
-    main(args.type, args.seed, args.episodes)
+    if args.env_config is not None:
+        env_config = dfaas_utils.json_to_dict(args.env_config)
+    else:
+        env_config = {}
+        print("WARNING: No given env config, using default!")
+
+    for seed in args.seeds:
+        main(env_config, seed, args.episodes)
+        print()
